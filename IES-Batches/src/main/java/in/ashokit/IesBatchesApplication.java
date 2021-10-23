b2abf978fee49ed1909faaf6b53b43b34c8e2d9b
Durgashankar <Durgashankar@DESKTOP-SN4HL7E>
2021-10-23 21:03:23 +0530

    Main Project

---
+++ IES-Batches/src/main/java/in/ashokit/IesBatchesApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IesBatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IesBatchesApplication.class, args);
+	}
+
+}
